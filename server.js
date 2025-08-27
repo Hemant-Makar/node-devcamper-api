7101bca6cdcb08cc103e4cc91bb8cc7eb5f265b0
Hemant Makar <hemant.makar@gmail.com>
2025-08-27 15:03:28 +0530

    Added error, async middleware and Error Handler for Bootcamps

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
+const errorHandler = require("./middleware/error");
 
 // Connect to database
 connectDB();
@@ -24,6 +25,7 @@ if (process.env.NODE_ENV === "development") {
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
