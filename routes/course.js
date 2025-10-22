cca33ccea0bc29ee3d61666079c82dd8354e0dd1
Hemant Makar <hemant.makar@gmail.com>
2025-10-22 15:16:31 +0530

    Course module completed

--- routes/course.js
+++ routes/course.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const route = express.Router();
-const { getCourses, getCourse } = require("../controllers/courses");
+const {
+  getCourses,
+  getCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} = require("../controllers/courses");
 
 route.route("/").get(getCourses);
-route.route("/:id").get(getCourse);
+route.route("/:id").get(getCourse).delete(deleteCourse).put(updateCourse);
+route.route("/bootcamps/:bootcampId/courses").post(createCourse);
 
 module.exports = route;
