e82d67008fda5c7935ba743cde23ec0582504175
Hemant Makar <hemant.makar@gmail.com>
2025-10-22 12:37:20 +0530

    Created Course model

---
+++ models/Course.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please add a course title"],
+    trim: true,
+    maxlength: [100, "Course title can not be more than 100 characters"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a course description"],
+    maxLength: [
+      1000,
+      "Course description can not be more than 1000 characters",
+    ],
+  },
+  weeks: {
+    type: String,
+    required: [true, "Please add number of weeks"],
+  },
+  tuition: {
+    type: Number,
+    required: [true, "Please add a tuition cost"],
+  },
+  minimumSkills: {
+    type: String,
+    required: [true, "Please add minimum skills"],
+    enum: ["beginner", "intermediate", "advanced"],
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  bootcamp: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Bootcamp",
+    required: true,
+  },
+});
+export default mongoose.model("Course", CourseSchema);
